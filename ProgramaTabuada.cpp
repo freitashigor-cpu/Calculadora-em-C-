797fb3feaa2f2cac02dfea9b1db5a323600adf92
Higor <freitashigor072@gmail.com>
2026-04-25 19:59:40 -0300

    Refactor ProgramaTabuada to include operation selection

--- ProgramaTabuada.cpp
+++ ProgramaTabuada.cpp
@@ -3,19 +3,30 @@
 using namespace std;
 int main(){
 	setlocale(LC_ALL, "Portuguese" );
+	string veze , resposta;
+	
 	float num;
 	float s;
+	
 	int tabuada;
 	int cont;
 	int vezes ;
-	int lim;
+	int lim; 
+
+	cout << "Voce deseja cacular vezes , soma , divisao ou subtraçao ?";
+	cout << "Digite veze pra fazer vezes , digite soma para somar , digite divisao pra divisao , digite subtrair pra subtrair";
+	cin >> resposta;
+	
 	cont = 0;
 	vezes = 0;
 	tabuada = 0;
+	
+	while(resposta == veze){
 	cout << "Digite um numero inteiro :";
 	cin >> num ;
 	cout << "Digite um limitador pra Tabuada :";
-	cin >> lim;
+	cin >> lim;}
+	
 	while(cont <= lim){
 		tabuada = num * vezes;
 		cout << "\n Tabuada do numero digitado : \n " << num << "*" << vezes << "=" << tabuada;
